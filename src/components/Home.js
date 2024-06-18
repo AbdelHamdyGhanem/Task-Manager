7d88f4c2b5c16bf65d483a5d76fed7b61a6c04c7
abdel ghanem <abdelghanem@abdels-laptop.home>
2024-06-18 13:19:12 -0300

    more style

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,24 @@ const Home = ({ user }) => {
   return (
     <div className="home-container">
       <div className="content">
-        {/* Your task columns or sections */}
+        <div className="progress-columns">
+          <div className="column">
+            <h3>Not Started</h3>
+            {/* Tasks for "Not Started" */}
+          </div>
+          <div className="column">
+            <h3>In Progress</h3>
+            {/* Tasks for "In Progress" */}
+          </div>
+          <div className="column">
+            <h3>Needs Review</h3>
+            {/* Tasks for "Needs Review" */}
+          </div>
+          <div className="column">
+            <h3>Done</h3>
+            {/* Tasks for "Done" */}
+          </div>
+        </div>
       </div>
       {user && (
         <div className="user-profile">
